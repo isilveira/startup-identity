c9fb5644509db9cbdaff9371693508e3436386f5
Ítalo Silveira <italobrian@gmail.com>
2020-01-18 02:49:31 -0300

    Add Core.Domain.Interfaces

---
+++ src/StartupIdentity.Core.Domain.Interfaces/Infrastructures/Data/IStartupIdentityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StartupIdentity.Core.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StartupIdentity.Core.Domain.Interfaces.Infrastructures.Data
+{
+    public interface IStartupIdentityDbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Role> Roles { get; set; }
+
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        int SaveChanges();
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
--- src/StartupIdentity.Infrastructures.Data/StartupIdentityDbContext.cs
+++ src/StartupIdentity.Infrastructures.Data/StartupIdentityDbContext.cs
@@ -1,11 +1,16 @@
 ﻿using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
+using StartupIdentity.Core.Domain.Entities;
+using StartupIdentity.Core.Domain.Interfaces.Infrastructures.Data;
 using System;
 
 namespace StartupIdentity.Infrastructures.Data
 {
-    public class StartupIdentityDbContext : DbContext
+    public class StartupIdentityDbContext : DbContext, IStartupIdentityDbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
         public StartupIdentityDbContext([NotNull] DbContextOptions options) : base(options)
         {
         }
