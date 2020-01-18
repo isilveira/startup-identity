faab988d31a4be3dc4d9d1237c86b4188be88eae
Ítalo Silveira <italobrian@gmail.com>
2020-01-18 02:39:55 -0300

    Adding libraries

--- src/StartupIdentity.Infrastructures.Data/StartupIdentityDbContext.cs
+++ src/StartupIdentity.Infrastructures.Data/StartupIdentityDbContext.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace StartupIdentity.Infrastructures.Data
 {
     public class StartupIdentityDbContext : DbContext
     {
+        public StartupIdentityDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        protected StartupIdentityDbContext()
+        {
+        }
     }
 }
