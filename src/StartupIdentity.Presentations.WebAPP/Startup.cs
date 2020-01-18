f320460c1814a8f7be8bff7b319f5473bf68fa36
Ítalo Silveira <italobrian@gmail.com>
2020-01-17 23:34:58 -0300

    Adding projects to solution

--- src/StartupIdentity.Presentations.WebAPP/Startup.cs
+++ src/StartupIdentity.Presentations.WebAPP/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StartupIdentity.Middleware.IoC;
 
 namespace StartupIdentity.Presentations.WebAPP
 {
@@ -16,6 +17,7 @@ namespace StartupIdentity.Presentations.WebAPP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMiddleware();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
