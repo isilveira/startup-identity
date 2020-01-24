e99647a2074977bb8280fca901a9329903cf4c11
Ítalo Silveira <italobrian@gmail.com>
2020-01-24 17:31:08 -0300

    Account SignIn

---
+++ src/StartupIdentity.Presentations.WebAPP/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StartupIdentity.Core.Domain.Entities;
+using StartupIdentity.Presentations.WebAPP.Models.Account;
+
+namespace StartupIdentity.Presentations.WebAPP.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AccountController> _logger;
+        private readonly IEmailSender _emailSender;
+        public AccountController(SignInManager<User> signInManager,
+            ILogger<AccountController> logger,
+            UserManager<User> userManager,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SignIn(string returnUrl = null)
+        {
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            var model = new AccountSignInViewModel(returnUrl ?? "~/", (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList());
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(AccountSignInViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(model.Credential, model.Password, model.RememberMe, lockoutOnFailure: true);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return Redirect(model.ReturnUrl);
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = model.ReturnUrl, RememberMe = model.RememberMe });
+                }
+                if (result.IsLockedOut)
+                {
+                    _logger.LogWarning("User account locked out.");
+                    return RedirectToPage("./Lockout");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult SignOut()
+        {
+            return View();
+        }
+    }
+}
