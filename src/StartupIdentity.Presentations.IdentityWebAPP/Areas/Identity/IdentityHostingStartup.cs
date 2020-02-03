621954deb0415b5e1ab53ec9e094f01047e9f164
Ítalo Silveira <italobrian@gmail.com>
2020-02-03 14:42:53 -0300

    Sample IdentityWebAPP

---
+++ src/StartupIdentity.Presentations.IdentityWebAPP/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(StartupIdentity.Presentations.IdentityWebAPP.Areas.Identity.IdentityHostingStartup))]
+namespace StartupIdentity.Presentations.IdentityWebAPP.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ src/StartupIdentity.Presentations.IdentityWebAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StartupIdentity.Presentations.IdentityWebAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
