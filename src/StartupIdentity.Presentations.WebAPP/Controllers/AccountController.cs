e75f76bbab09f6db0ddfcc1261493e55da19ad83
Ítalo Silveira <italobrian@gmail.com>
2020-02-04 18:18:00 -0300

    Adding mvc to WebAPP

---
+++ src/StartupIdentity.Presentations.WebAPP/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StartupIdentity.Presentations.WebAPP.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult SignOut()
+        {
+            return View();
+        }
+    }
+}
--- src/StartupIdentity.Presentations.WebAPP/Startup.cs
+++ src/StartupIdentity.Presentations.WebAPP/Startup.cs
@@ -5,17 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StartupIdentity.Middleware.IoC;
 
 namespace StartupIdentity.Presentations.WebAPP
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddStartupIdentityDbContexts(Configuration);
+            services.AddIdentityConfiguration();
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +40,13 @@ namespace StartupIdentity.Presentations.WebAPP
 
             app.UseRouting();
 
+            app.UseStartupIdentity();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
